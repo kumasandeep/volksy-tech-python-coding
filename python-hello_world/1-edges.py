d07aa399f769c61fa325a506f45cd8d982899da8
kumasandeep <ksandeep@brightcomgroup.com>
2022-12-23 10:47:29 +0000

    l

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print("First 3 letters : Hol")
-print("Last 2 letters : on")
-print("Middle word : olberto")
+print("First 3 letters: Hol")
+print("Last 2 letters: on")
+print("Middle word: olberto")
